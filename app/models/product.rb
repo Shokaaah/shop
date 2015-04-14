cdc28ea3bb2ea1be8854c2dd2365d1db5903a1c9
Shokaaah <mr.shok@mail.ru>
2015-04-14 22:25:42 +0300

    Validtion & some tests

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,9 @@
 class Product < ActiveRecord::Base
+	validates :title , :description , :image_url, :price , presence:true 
+	validates :price , numericality:{greater_than_or_equal_to: 0.01}
+	validates :title , uniqueness:true
+	validates :image_url , allow_blank:true , format: { 
+		with: %r{.jpg|.jpeg|.gif|.png|.bmp\Z}i,
+		message: "URL must end with .jpg;.jpeg;.gif;.png"
+	}
 end
